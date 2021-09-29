12d32838f1cd1c29af7047d70bb4c9cf7b6e27e9
wangke <1015005300@qq.com>
2021-09-29 17:50:09 +0800

    add hello world controller

--- main.go
+++ main.go
@@ -3,5 +3,26 @@ package main
 import "github.com/beego/beego/v2/server/web"
 
 func main() {
-	web.Run()
+	ctrl := &MainController{}
+	web.Router("/hello", ctrl)
+
+	web.Run("127.0.0.1:8089")
+}
+
+// MainController:
+// The controller must implement ControllerInterface
+// Usually we extends web.Controller
+type MainController struct {
+	web.Controller
+}
+
+// address: http://localhost:8080 GET
+func (ctrl *MainController) Get() {
+
+	name := ctrl.GetString("name")
+	if name == "" {
+		ctrl.Ctx.WriteString("Hello World")
+		return
+	}
+	ctrl.Ctx.WriteString("Hello " + name)
 }
